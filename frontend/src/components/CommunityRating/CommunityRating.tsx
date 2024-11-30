739d5bfe5658615a1047bcbac66c1c148e997d07
Timothy Zhou <z5589644@ad.unsw.edu.au>
2024-11-30 22:45:05 +1100

    feat: implemented CommunityRating component for Tip page

---
+++ frontend/src/components/CommunityRating/CommunityRating.tsx
@@ -0,0 +1,40 @@
+interface CommunityRatingProps {
+  rating?: string;
+}
+
+const CommunityRating = (props: CommunityRatingProps) => {
+  const ratingComponentList = [];
+  let numRating = 0;
+  if (props.rating !== undefined) {
+    numRating = parseInt(props.rating);
+  }
+
+  // reset rating to min or max if invalid input
+  if (numRating >= 5) numRating = 5;
+  if (numRating < 0) numRating = 0;
+
+  // add filled / unfilled circle components
+  for (let i = 0; i < numRating; i++) {
+    ratingComponentList.push(
+      <div className="m-1 flex h-5 w-5 rounded-full border border-black bg-[#63C779]"></div>
+    );
+  }
+  for (let i = 0; i < 5 - numRating; i++) {
+    ratingComponentList.push(
+      <div className="m-1 flex h-5 w-5 rounded-full border border-black bg-white"></div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col justify-center">
+      <h3 className="m-auto text-lg font-semibold text-[#555555]">
+        Community Rating
+      </h3>
+      <div className="flex flex-row justify-center">
+        {ratingComponentList}
+      </div>
+    </div>
+  );
+};
+
+export default CommunityRating;
--- frontend/src/pages/Tip.tsx
+++ frontend/src/pages/Tip.tsx
@@ -1,4 +1,5 @@
 import TipHeading from "../components/TipHeading";
+import CommunityRating from "../components/CommunityRating";
 
 export const Tip = () => {
   return (
@@ -6,8 +7,13 @@ export const Tip = () => {
       id="tip-page-container"
       className="flex h-full items-center justify-center border border-red-400"
     >
-      <div id="tip-post-container">
-        <TipHeading>This is my tip</TipHeading> 
+      <div id="tip-post-container" className="flex justify-center w-1/2 border border-red-400">
+        <div className="inline-block ml-0 mr-auto border border-blue-400 w-4/5">
+          <TipHeading>This is my tip hello hello hello hello</TipHeading> 
+        </div>
+        <div className="inline-block ml-auto mr-0 my-auto w-1/5 border border-purple-400">
+          <CommunityRating rating="3" />
+        </div>
       </div>
     </div>
   );
