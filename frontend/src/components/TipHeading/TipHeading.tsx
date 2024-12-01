9132a14401a3a649426574a08a631b5a5e026115
Natalie Chen <z5592383@ad.unsw.edu.au>
2024-12-01 14:35:09 +1100

    Make tip heading size smaller

--- frontend/src/components/TipHeading/TipHeading.tsx
+++ frontend/src/components/TipHeading/TipHeading.tsx
@@ -7,7 +7,7 @@ interface TipHeadingProps {
 const TipHeading = (props: TipHeadingProps) => {
   return (
     <div>
-      <h1 className="text-6xl font-bold text-[#63C779]">
+      <h1 className="text-5xl font-bold text-[#63C779]">
         {(props.children as String).toUpperCase()}
       </h1>
     </div>
