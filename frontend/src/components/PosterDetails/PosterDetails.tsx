71e3b4112d1f2f7cf75aea247fe8c26ff1777ce3
Natalie Chen <z5592383@ad.unsw.edu.au>
2024-12-01 16:34:02 +1100

    Adjust PosterDetails component so that image is centred vertically

--- frontend/src/components/PosterDetails/PosterDetails.tsx
+++ frontend/src/components/PosterDetails/PosterDetails.tsx
@@ -9,17 +9,15 @@ interface PosterDetailsObj {
 const PosterDetails = (props: PosterDetailsObj) => {
 
     return (
-        <div className="p-2 flex items-start space-x-4 mt-3">
-            <div className="flex items-center">
-                <img
-                    src={props.profilePic || "https://i.pinimg.com/550x/18/b9/ff/18b9ffb2a8a791d50213a9d595c4dd52.jpg"}
-                    className="w-8 h-8 rounded-full mb-2 mr-4"
-                />
-                <h3 className="underline text-base mr-1">{props.name}</h3>
-                <h3 className="text-base">·&nbsp;</h3>
-                <Date date={props.date} />
-              
-            </div>
+        <div className="items-start flex items-center border mt-5">
+            <img
+                src={props.profilePic || "https://i.pinimg.com/550x/18/b9/ff/18b9ffb2a8a791d50213a9d595c4dd52.jpg"}
+                className="w-8 h-8 rounded-full mr-4"
+            />
+            <h3 className="underline text-base mr-1">{props.name}</h3>
+            <h3 className="text-base">·&nbsp;</h3>
+            <Date date={props.date} />
+            
         </div>
     );
 };
