568def571aeeb7764313004ee7b2865f3e71ae3a
Jayden Nguyen <jayden.m.nguyen@student.unsw.edu.au>
2024-12-05 22:20:38 +1100

    added user folder

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -8,9 +8,11 @@ import {
 } from "@firebase/firestore";
 import { query, Router } from "express";
 import DB from "../db/db";
+import { register } from "../user/register";
 
 const userRouter = Router();
 
+// not correct code
 userRouter.get("/:id", (req, res) => {
   console.log(req.params.id);
   res.send({
@@ -19,18 +21,14 @@ userRouter.get("/:id", (req, res) => {
 });
 
 userRouter.post("/register", async (req, res) => {
-  const ret = await addDoc(collection(DB, "users"), {
-    email: req.body.email,
-    favouritePosts: [],
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    password: req.body.password,
-    profileUrl: "",
-  });
+  const ret = await register(
+    req.body.email,
+    req.body.password,
+    req.body.firstName,
+    req.body.lastName
+  );
 
-  res.send({
-    userId: ret.id,
-  });
+  res.send(ret);
 });
 
 userRouter.delete("/:id", async (req, res) => {
