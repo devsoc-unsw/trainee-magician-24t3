09138e43b9d1c7861e1764d0de3b5f5173a0e58f
Natalie Chen <z5592383@ad.unsw.edu.au>
2024-12-05 23:52:24 +1100

    Implement user login function

---
+++ backend/src/user/login.ts
@@ -0,0 +1,24 @@
+import { addDoc, collection, query, where, getDocs, limit } from "@firebase/firestore";
+import DB from "../db/db";
+
+export interface LoginReturn {
+  userId: string;
+}
+
+export async function login(
+  email: string,
+  password: string,
+): Promise<LoginReturn> {
+  const usersRef = collection(DB, 'users');
+
+  const q = query(usersRef, where('email', '==', email), where('password', '==', password), limit(1));
+  const querySnapshot = await getDocs(q);
+  
+  if (querySnapshot.empty) {
+    throw new Error('Invalid email or password');
+  }
+
+  return {
+    userId: querySnapshot.docs[0].id
+  }
+}
